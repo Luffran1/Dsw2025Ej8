e4d4133fb3f66589d395a47eb764399f627e9ff4
bayzon <juanignaciobizone@gmail.com>
2025-05-01 22:22:18 -0300

    Errores de Clase CuentaCorriente corregidos

--- Domain/CuentaCorriente.cs
+++ Domain/CuentaCorriente.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using static Dsw2025Ej8.Domain.Excepciones;
 
-namespace Dsw2025Ej8.Domain
+namespace Dsw2025Ej8.Domain;
+
+internal class CuentaCorriente : CuentaBancaria
 {
-    internal class CuentaCorriente
+
+    public decimal _comision { get; set; }
+    public decimal _limiteDeDescubierto { get; set; }
+
+
+
+    public CuentaCorriente(string numero, decimal saldo, string[] titulares, Estado estado, decimal comision) : base(numero, saldo, titulares)
     {
+        _comision = comision;
+
     }
+    public override void Depositar(decimal monto)
+    {
+        if (monto <= 0)
+            throw new MontoNoValido();
+        if (_estado != Estado.Activa)
+            throw new CuentaNoActiva(_estado);
+        monto -= monto * _comision;
+        _saldo += monto;
+    }
+    public override void Retirar(decimal monto)
+    {
+        if (monto <= 0)
+            throw new MontoNoValido();
+        if (_estado != Estado.Activa)
+            throw new CuentaNoActiva(_estado);
+
+        if (_saldo - monto >= -_limiteDeDescubierto)
+        {
+            _saldo -= monto;
+        }
+        else
+        {
+            _estado = Estado.Suspendida;
+            throw new SaldoInsuficiente();
+        }
+        if (_saldo < monto)
+        {
+            _estado = Estado.Suspendida;
+        }
+
+    }
+
 }
